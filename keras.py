0e8eb8eaac510408a1743ca60a55203173e6d350
nik1998 <beregnovnikita@gmail.com>
2020-08-24 10:13:15 +0300

    Add keras examples

---
+++ keras.py
@@ -0,0 +1,159 @@
+from os.path import join
+
+image_dir = '../input/dog-breed-identification/train/'
+img_paths = [join(image_dir, filename) for filename in
+                           ['0c8fe33bd89646b678f6b2891df8a1c6.jpg',
+                            '0c3b282ecbed1ca9eb17de4cb1b6e326.jpg',
+                            '04fb4d719e9fe2b6ffe32d9ae7be8a22.jpg',
+                            '0e79be614f12deb4f7cae18614b7391b.jpg']]
+
+import numpy as np
+from tensorflow.python.keras.applications.resnet50 import preprocess_input
+from tensorflow.python.keras.preprocessing.image import load_img, img_to_array
+
+image_size = 224
+
+def read_and_prep_images(img_paths, img_height=image_size, img_width=image_size):
+    imgs = [load_img(img_path, target_size=(img_height, img_width)) for img_path in img_paths]
+    img_array = np.array([img_to_array(img) for img in imgs])
+    output = preprocess_input(img_array)
+    return(output)
+
+preds = my_model.predict(test_data)
+#imagenet
+from learntools.deep_learning.decode_predictions import decode_predictions
+from IPython.display import Image, display
+
+most_likely_labels = decode_predictions(preds, top=3, class_list_path='../input/resnet50/imagenet_class_index.json')
+
+for i, img_path in enumerate(img_paths):
+    display(Image(img_path))
+    print(most_likely_labels[i])
+
+
+from tensorflow.python.keras.applications import ResNet50
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Flatten, GlobalAveragePooling2D
+
+num_classes = 2
+resnet_weights_path = '../input/resnet50/resnet50_weights_tf_dim_ordering_tf_kernels_notop.h5'
+
+my_new_model = Sequential()
+my_new_model.add(ResNet50(include_top=False, pooling='avg', weights=resnet_weights_path))
+my_new_model.add(Dense(num_classes, activation='softmax'))
+
+# Say not to train first layer (ResNet) model. It is already trained
+my_new_model.layers[0].trainable = False
+my_new_model.compile(optimizer='sgd', loss='categorical_crossentropy', metrics=['accuracy'])
+
+from tensorflow.python.keras.applications.resnet50 import preprocess_input
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+
+image_size = 224
+data_generator = ImageDataGenerator(preprocessing_function=preprocess_input)
+
+
+train_generator = data_generator.flow_from_directory(
+        '../input/urban-and-rural-photos/rural_and_urban_photos/train',
+        target_size=(image_size, image_size),
+        batch_size=24,
+        class_mode='categorical')
+
+validation_generator = data_generator.flow_from_directory(
+        '../input/urban-and-rural-photos/rural_and_urban_photos/val',
+        target_size=(image_size, image_size),
+        class_mode='categorical')
+
+my_new_model.fit_generator(
+        train_generator,
+        steps_per_epoch=3,
+        epochs=2,
+        validation_data=validation_generator,
+        validation_steps=1)
+
+
+my_new_model.compile(optimizer='sgd',
+                     loss='categorical_crossentropy',
+                     metrics=['accuracy'])
+
+# augmentation
+
+
+data_generator_with_aug = ImageDataGenerator(preprocessing_function=preprocess_input,
+                                   horizontal_flip=True,
+                                   width_shift_range = 0.2,
+                                   height_shift_range = 0.2)
+
+# horizontal_flip - two images (+mirror image)
+# width_shift_range, height_shift_range - random 0..0.2 per sent
+
+import numpy as np
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from tensorflow.python import keras
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, Flatten, Conv2D, Dropout
+
+
+img_rows, img_cols = 28, 28
+num_classes = 10
+
+def data_prep(raw):
+    out_y = keras.utils.to_categorical(raw.label, num_classes)
+
+    num_images = raw.shape[0]
+    x_as_array = raw.values[:,1:]
+    x_shaped_array = x_as_array.reshape(num_images, img_rows, img_cols, 1)
+    out_x = x_shaped_array / 255
+    return out_x, out_y
+
+train_file = "../input/digit-recognizer/train.csv"
+raw_data = pd.read_csv(train_file)
+
+x, y = data_prep(raw_data)
+
+model = Sequential()
+model.add(Conv2D(20, kernel_size=(3, 3),
+                 activation='relu',
+                 input_shape=(img_rows, img_cols, 1)))
+model.add(Conv2D(20, kernel_size=(3, 3), activation='relu'))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer='adam',
+              metrics=['accuracy'])
+model.fit(x, y,
+          batch_size=128,
+          epochs=2,
+          validation_split = 0.2)
+
+# Stride lengths and Dropout
+# stride - step through which we go
+model = Sequential()
+model.add(Conv2D(30, kernel_size=(3, 3),
+                 strides=2,
+                 activation='relu',
+                 input_shape=(img_rows, img_cols, 1)))
+model.add(Dropout(0.5))
+model.add(Conv2D(30, kernel_size=(3, 3), strides=2, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dense(num_classes, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer='adam',
+              metrics=['accuracy'])
+model.fit(x, y,
+          batch_size=128,
+          epochs=2,
+          validation_split = 0.2)
+
+pretrained_model = tf.keras.applications.VGG16(
+        weights='imagenet',
+        include_top=False ,
+        input_shape=[*IMAGE_SIZE, 3]
+    )
+    pretrained_model.trainable = False
